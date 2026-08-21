9d3283900a4e357c9e0dca41bfee0171319abd2d
Rudra Parmar <rudraparmar203@gmail.com>
2026-08-21 15:40:28 +0530

    chore(M4): integrate M1 Identity foundation

---
+++ Source/AIITSM/AIITSM.Infrastructure/01_M1_IdentityAccess/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AITSM.Infrastructure._01_M1_IdentityAccess.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+    }
+}
